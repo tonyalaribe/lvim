3868336716158e7c0783f307ad6206cd1e083176
Anthony Alaribe <anthonyalaribe@gmail.com>
2022-03-21 10:38:54 +0100

    updated config

--- config.lua
+++ config.lua
@@ -11,8 +11,8 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
--- lvim.colorscheme = "onedarker"
-lvim.colorscheme = "onedarkpro"
+lvim.colorscheme = "onedarker"
+--lvim.colorscheme = "onedarkpro"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
@@ -21,40 +21,28 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["gt"] = ":pop<cr>"
 lvim.keys.normal_mode["K"] = ":lua require'lspsaga.hover'.render_hover_doc()<CR>"
 
--- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = ""
--- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+  -- for input mode
+  i = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev,
+  },
+  -- for normal mode
+  n = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+  },
+}
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
--- }
+
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -108,110 +96,33 @@ vim.cmd([[
   let g:neoformat_enabled_haskell = ['ormolu']
 
 
-" delete should not cut data. <leader>d can be used the way d was used previously
-nnoremap x "_x
-nnoremap d "_d
+  " delete should not cut data. <leader>d can be used the way d was used previously
+  nnoremap x "_x
+  nnoremap d "_d
 
-" nnoremap D "_D
-vnoremap d "_d
+  " nnoremap D "_D
+  vnoremap d "_d
 
-" Vmap for maintain Visual Mode after shifting > and <
-vmap < <gv
-vmap > >gv
+  " Vmap for maintain Visual Mode after shifting > and <
+  vmap < <gv
+  vmap > >gv
 
-" vim.cmd('let g:nvim_tree_git_hl = 1')
-nmap <silent> <F3> :NvimTreeToggle<CR>
+  " vim.cmd('let g:nvim_tree_git_hl = 1')
+  nmap <silent> <F3> :NvimTreeToggle<CR>
 
-hi Normal guibg=NONE ctermbg=NONE
+  " automatically rebalance windows on vim resize
+  autocmd VimResized * :wincmd =
+
+  " zoom a vim pane, <C-w>= to re-balance
+  nnoremap <leader>- :wincmd _<cr>:wincmd \|<cr>
+  nnoremap <leader>= :wincmd =<cr>
+
 ]])
-
--- generic LSP settings
-
--- ---@usage disable automatic installation of servers
--- lvim.lsp.automatic_servers_installation = false
-
--- ---@usage Select which servers should be configured manually. Requires `:LvimCacheRest` to take effect.
--- See the full default list `:lua print(vim.inspect(lvim.lsp.override))`
--- vim.list_extend(lvim.lsp.override, { "pyright" })
-
--- ---@usage setup a server -- see: https://www.lunarvim.org/languages/#overriding-the-default-configuration
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pylsp", opts)
-
--- you can set a custom on_attach function that will be used for all the language servers
--- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
--- you can overwrite the null_ls setup table (useful for setting the root_dir function)
--- lvim.lsp.null_ls.setup = {
---   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
--- }
--- or if you need something more advanced
--- lvim.lsp.null_ls.setup.root_dir = function(fname)
---   if vim.bo.filetype == "javascript" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
---       or require("lspconfig/util").path.dirname(fname)
---   elseif vim.bo.filetype == "php" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
---   else
---     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
---   end
--- end
-
--- -- set a formatter, this will override the language server formatting capabilities (if it exists)
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { exe = "black", filetypes = { "python" } },
---   { exe = "isort", filetypes = { "python" } },
---   {
---     exe = "prettier",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     args = { "--print-with", "100" },
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
-
--- -- set additional linters
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { exe = "flake8", filetypes = { "python" } },
---   {
---     exe = "shellcheck",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     args = { "--severity", "warning" },
---   },
---   {
---     exe = "codespell",
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "javascript", "python" },
---   },
--- }
 
 -- Additional Plugins
 lvim.plugins = {
-	{ "folke/tokyonight.nvim" },
-	{
-		"folke/trouble.nvim",
-		cmd = "TroubleToggle",
-	},
-	-- {
-	-- 	"wfxr/minimap.vim",
-	-- 	run = "cargo install --locked code-minimap",
-	-- 	-- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
-	-- 	config = function()
-	-- 		vim.cmd("let g:minimap_width = 10")
-	-- 		vim.cmd("let g:minimap_auto_start = 1")
-	-- 		vim.cmd("let g:minimap_auto_start_win_enter = 1")
-	-- 	end,
-	-- },
+	{"folke/tokyonight.nvim" },
+	{"folke/trouble.nvim",cmd = "TroubleToggle"},
 	{
 		"tpope/vim-fugitive",
 		cmd = {
@@ -232,30 +143,11 @@ lvim.plugins = {
 		ft = { "fugitive" },
 	},
   {'sbdchd/neoformat'},
-	{
-		"sindrets/diffview.nvim",
-		event = "BufRead",
-	},
-	{
-		"folke/lsp-colors.nvim",
-		event = "BufRead",
-	},
-	{
-		"ray-x/lsp_signature.nvim",
-		event = "BufRead",
-		config = function()
-			require("lsp_signature").setup()
-		end,
-	},{
-		"tami5/lspsaga.nvim",
-		config = function()
-			local saga = require("lspsaga")
-		end,
-	},
-	{
-		"simrat39/symbols-outline.nvim",
-		cmd = "SymbolsOutline",
-	},
+	{"sindrets/diffview.nvim",event = "BufRead"},
+	{"folke/lsp-colors.nvim",event = "BufRead"},
+	{"ray-x/lsp_signature.nvim",event = "BufRead",config = function() require("lsp_signature").setup() end},
+  {"tami5/lspsaga.nvim"},
+	{"simrat39/symbols-outline.nvim",cmd = "SymbolsOutline"},
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		event = "BufRead",
@@ -272,9 +164,11 @@ lvim.plugins = {
 	{ "michaelb/sniprun", run = "bash ./install.sh" },
 	{ "ckipp01/stylua-nvim" },
   { "github/copilot.vim" },
-  { "olimorris/onedarkpro.nvim" },
   { "neovimhaskell/haskell-vim" },
   { "nathom/filetype.nvim" },
+  { "beauwilliams/focus.nvim", config = function() require("focus").setup() end },
+  { "christoomey/vim-tmux-navigator"},
+  {"romgrk/nvim-treesitter-context"},
 }
 
 lvim.builtin.which_key.mappings["t"] = {
@@ -290,20 +184,27 @@ lvim.builtin.which_key.mappings["t"] = {
 lvim.builtin.which_key.mappings["i"] = {
 	name = "Saga",
 	r = { "<cmd>lua require'lspsaga.rename'.rename()<CR>", "Rename" },
-  i = {"<cmd>lua require'lspsaga.hover'.render_hover_doc()<CR>", "Hover Doc"}
-
+  i = { "<cmd>lua require'lspsaga.hover'.render_hover_doc()<CR>", "Hover Doc"},
+  f = { "<cmd>lua require'lspsaga.provider'.lsp_finder()<CR>", "Find Via LSP" },
+  a = { "<cmd>lua require('lspsaga.codeaction').code_action()<CR>", "Code action"},
+  k = { "<cmd>lua require'lspsaga.provider'.preview_definition()<CR>", "Preview Definition"},
+  d = { "<cmd>lua require'lspsaga.diagnostic'.show_line_diagnostics()<CR>", "line diagnostic"},
 }
+
+lvim.builtin.which_key.mappings["sB"] = {
+  "<cmd>lua require'telescope.builtin'.buffers{}<CR>", "Buffers"
+}
+
+vim.cmd([[
+  " jump diagnostic
+  nnoremap <silent> [e <cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev()<CR>
+  nnoremap <silent> ]e <cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_next()<CR>
+]])
 
 
 lvim.builtin.which_key.mappings["z"] = {
 	z = { '<cmd>lua require("stylua-nvim").format_file()<CR>', "Format Lua" },
 }
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- lvim.autocommands.custom_groups = {
---   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
--- }
-
-require('onedarkpro').load()
 
 require('lspconfig').tailwindcss.setup({
   filetypes = {"haskell", "html", "javascript"},
