c83adc816b1a830afa6ea6b868154e0e74c617a4
Anthony Alaribe <anthonyalaribe@gmail.com>
2022-10-11 14:01:05 +0200

    remove indentlines

--- config.lua
+++ config.lua
@@ -209,18 +209,6 @@ lvim.plugins = {
   end
   },
   {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "BufRead",
-    setup = function()
-      vim.g.indentLine_enabled = 1
-      vim.g.indent_blankline_char = "▏"
-      vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard" }
-      vim.g.indent_blankline_buftype_exclude = { "terminal" }
-      vim.g.indent_blankline_show_trailing_blankline_indent = false
-      vim.g.indent_blankline_show_first_indent_level = false
-    end,
-  },
-  {
     "simrat39/rust-tools.nvim",
     config = function()
       local status_ok, rust_tools = pcall(require, "rust-tools")
