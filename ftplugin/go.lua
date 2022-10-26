486cd483c15a5bfe281cf100e6b23acd6a7d3c2f
Anthony Alaribe <anthonyalaribe@gmail.com>
2022-10-26 23:09:00 +0200

    golang

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -32,10 +32,10 @@ local opts = {
   capabilities = require("lvim.lsp").common_capabilities(),
 }
 
-local servers = require "nvim-lsp-installer.servers"
-local server_available, requested_server = servers.get_server "gopls"
-if server_available then
-  opts.cmd_env = requested_server:get_default_options().cmd_env
-end
+-- local servers = require "nvim-lsp-installer.servers"
+-- local server_available, requested_server = servers.get_server "gopls"
+-- if server_available then
+--   opts.cmd_env = requested_server:get_default_options().cmd_env
+-- end
 
 require("lvim.lsp.manager").setup("gopls", opts)
